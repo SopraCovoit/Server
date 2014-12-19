60d5a60628c394020e48602881c544c68044ea28
realsoc <hugo.djemaa@gmail.com>
2014-12-19 12:01:03 +0100

    Servletallrunningmaggle

--- web/WEB-INF/src/helloWorld.java
+++ web/WEB-INF/src/helloWorld.java
@@ -7,13 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class helloWorld extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-        System.out.println("test");
+
+        // Set the response message's MIME type
+        response.setContentType("text/html;charset=UTF-8");
+        // Allocate a output writer to write the response message into the network socket
+        PrintWriter out = response.getWriter();
     }
 
 }
