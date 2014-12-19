a62d23b0231945072819fb58eae1379497ea2c85
Name-less <ph0enix.fiire@gmail.com>
2014-12-19 16:43:32 +0100

    DAO crées, mais a implémenter

---
+++ web/WEB-INF/src/model/bdd/DAO_Path.java
@@ -0,0 +1,12 @@
+package model.bdd;
+
+/**
+ * Created by julescantegril on 19/12/2014.
+ */
+public class DAO_Path extends DAO {
+
+    public DAO_Path(String urlBase) {
+        super(urlBase);
+    }
+
+}
